f826eb9b046766abb71e7f171b16f7bb8a803672
Siva <siva@tess-pc>
2026-07-17 14:01:42 +0530

    updated footer

--- script.js
+++ script.js
@@ -92,7 +92,10 @@ function goToContact(e) {
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
     anchor.addEventListener('click', function (e) {
         const targetId = this.getAttribute('href');
-        if (!targetId || targetId === '#') return;
+        if (!targetId || targetId === '#') {
+            e.preventDefault();
+            return;
+        }
 
         if (targetId === '#contact') {
             goToContact(e);
